6fe8ad3ef978c887ab148438c9e048e3ebcba0bf
Kamilla <121770492+kamcso@users.noreply.github.com>
2025-03-07 14:40:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,49 @@
 import streamlit as st
+import datetime
+import requests
+import pandas as pd
+import numpy as np
 
-st.secrets['apikey']
+'''
+# TaxiFareModel front
 
-st.markdown('# Hello this is my first website')
+This front queries the Le Wagon [taxi fare model API](https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2)
+'''
 
-st.header('This is a header')
+with st.form(key='params_for_api'):
 
-st.write('This is text')
+    pickup_date = st.date_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
+    pickup_time = st.time_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
+    pickup_datetime = f'{pickup_date} {pickup_time}'
+    pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
+    pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
+    dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
+    dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
+    passenger_count = st.number_input('passenger_count', min_value=1, max_value=8, step=1, value=1)
 
-columns = st.columns(2)
+    st.form_submit_button('Make prediction')
 
-with columns[0]:
-    if st.checkbox('This is a checkbox'):
-        st.write('The checkbox is ticked')
+params = dict(
+    pickup_datetime=pickup_datetime,
+    pickup_longitude=pickup_longitude,
+    pickup_latitude=pickup_latitude,
+    dropoff_longitude=dropoff_longitude,
+    dropoff_latitude=dropoff_latitude,
+    passenger_count=passenger_count)
 
-    if st.button('This is a button'):
-        st.write('The button is clicked')
+wagon_cab_api_url = 'https://taxifare.lewagon.ai/predict'
+response = requests.get(wagon_cab_api_url, params=params)
 
-    st.image('peru.jpg', width=200)
+prediction = response.json()
 
-with columns[1]:
-    value = st.slider('This is a slider')
+pred = prediction['fare']
 
-    if value:
-        st.write(value * 1000)
+st.header(f'Fare amount: ${round(pred, 2)}')
 
-    st.date_input('date', value='today')
+
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
+st.map(df)
